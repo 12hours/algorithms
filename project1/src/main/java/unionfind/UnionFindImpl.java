91b79332f0f8d2f94193e19a3707bbae6b51bd9f
12hours <decl123>
2017-05-09 17:00:46 +0300

    implement of uf

---
+++ project1/src/main/java/unionfind/UnionFindImpl.java
@@ -0,0 +1,30 @@
+package unionfind;
+
+public class UnionFindImpl implements UnionFind {
+	private int N;
+	
+	public UnionFindImpl(int N) {
+		this.N = N;
+	}
+	
+	public void union(int p, int q) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean connected(int p, int q) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int find(int p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
