4301d710ee874e49aab454124f0b17bb877ed557
12hours <decl123>
2017-05-22 20:01:58 +0300

    shell sort unimplemented

---
+++ project2/src/main/java/classes/sortings/ShellSort.java
@@ -0,0 +1,12 @@
+package classes.sortings;
+
+/**
+ * Created by nm on 22.5.17.
+ */
+//TODO implement ShellSort
+public class ShellSort implements IntArraySort {
+    @Override
+    public void sort(int[] array) {
+
+    }
+}
